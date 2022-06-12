071e631b49d22637a3c9076d68e8921eab9f11c8
bibinilo <102391085+bibinilo@users.noreply.github.com>
2022-06-12 18:25:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main ()
-{
-    
-    return 0;
+int main(void) {  
+  char PalavraVelha[100];
+  int Tamanho = 0;
+  char LetraVelha;
+  char LetraNova;
+  char PalavraNova[100];
+  
+  printf("Usuario, insira a palavra\n");
+  scanf("%s", PalavraVelha);
+
+  printf("Usuario, escolha uma letra a ser substituida\n");
+  scanf("%s", LetraVelha);
+
+  printf("Usuario, escolha uma letra para substituir\n");
+  scanf("%s", LetraNova);
+
+  TrocaCaractere (&PalavraVelha, &Tamanho, LetraVelha, LetraNova, &PalavraNova);
+
+  int quantidade = ContaTrocas (PalavraVelha, LetraVelha);
+
+  printf("%s \n %s \n %i",PalavraNova, PalavraVelha, quantidade);
+
+  return 0;
 }
