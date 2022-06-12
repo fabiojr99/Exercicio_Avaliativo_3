7dc113eae5a891e2f5e67b4ef346d64c6acc9847
fabiojr99 <102391084+fabiojr99@users.noreply.github.com>
2022-06-12 19:16:12 -0300

    Update MinhaBiblioteca.h

--- MinhaBiblioteca.h
+++ MinhaBiblioteca.h
@@ -1,8 +1,8 @@
 #ifndef __MINHABIBLIOTECA_H
 #define __MINHABIBLIOTECA_H
 
-int ContaTrocas (char mensagem_inicial[], int contador_de_trocas);
+int ContaTrocas (char mensagem_inicial[], char caractere_trocado);
 
-void TrocaCaractere (char* mensagem_inicial[], int contador_de_elementos, char caractere_velho, char caractere_novo, char* mensagem_final[]);
+void TrocaCaractere (char* mensagem_inicial[], int* contador_de_elementos, char caractere_velho, char caractere_novo, char* mensagem_final[]);
 
 #endif
